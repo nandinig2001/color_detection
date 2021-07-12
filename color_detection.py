234ef9233a9fa4fd667cb498fd900ab930bf4749
nandinig2001 <gawankarnandini@gmail.com>
2021-07-12 21:08:01 +0530

    project completed

--- color_detection.py
+++ color_detection.py
@@ -3,7 +3,8 @@ import cv2
 
 # opencv stores in BGR format and not RGB format
 
-img_path = 'pic.jpg'
+img_path = '2.jpg'
+# img_path = '3.jpg'
 csv_path = 'colors.csv'
 
 index = ['color', 'color_name', 'hex', 'R', 'G', 'B']
@@ -15,16 +16,44 @@ img = cv2.resize(img, (800,600))
 clicked = False
 r = g = b = xposition = yposition = 0
 
+def get_color_name(R,G,B):
+    min = 1000
+    for i in range(len(dataframe)):
+        d = abs(R - int(dataframe.loc[i,'R'])) + abs(G - int(dataframe.loc[i,'G'])) + abs(B - int(dataframe.loc[i,'B']))
+        if d<= min:
+            min=d
+            colorname = dataframe.loc[i,'color_name']
+
+    return colorname
+
+
 def draw_function(event, x, y, flags, parameters):
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        print(x,y)
+        global clicked, r, g, b, xposition, yposition 
+        clicked = True
+        xposition= x
+        yposition = y
         b, g, r =img[y, x]
-        print(b,g,r)
+        b = int(b)
+        g = int(g)
+        r= int(r)
+
+cv2.namedWindow('COLOR DETECTOR')
+cv2.setMouseCallback('COLOR DETECTOR', draw_function)
+
+# print(clicked, r, g, b, xposition, yposition)
+
+while True:
+    cv2.imshow('COLOR DETECTOR', img)
+    if clicked :
+        cv2.rectangle(img, (20,20), (600,60), (b,g,r), -1)
+        text = get_color_name(r,g,b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+        cv2.putText(img,text, (50,50), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
+        if r + g + b >=600:
+            cv2.putText(img,text, (50,50), 2, 0.8, (255,255,255), 2, cv2.LINE_AA)
+    
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
 
 
-
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', draw_function)
-cv2.imshow('image', img)
-cv2.waitKey(0)
 cv2.destroyAllWindows()
